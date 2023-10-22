7fe8fc781a919e85a0aa25a600a41a92dcef1d9a
Rohit Saini <rs0118032@gmail.com>
2023-10-23 01:58:35 +0530

    fetching data from api done

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -3,26 +3,28 @@ import { api_id,api_key,type } from "./API_AUTH_KEYS";
 
 const Fetchdata = (props) => {
   const [recipies, setRecipies] = useState([]);
+  const [error, setError]= useEffect();
+
   const base_url= "https://api.edamam.com/api/recipes/v2";
   const auth= `app_id=${api_id}&app_key=${api_key}&type=${type}`;
 
   useEffect(() => {
     const getData = async () => {
       try{
+        let res={};
         if(props.id !== ""){ 
-            const res = await fetch(url+'/'+props.id+'?'+auth);
+            res = await fetch(url+'/'+props.id+'?'+auth);
         }
         if(props.q !==""){
-            const res = await fetch(`${url}?${type}&q=${props.q}&${auth}`);
+            res = await fetch(`${url}?${type}&q=${props.q}&${auth}`);
         }
         if (!res.ok) throw new Error("Oops! An error has occured");
         const json = await res.json();
         const data= json.hits;
         setRecipies(data);
-      } catch (error) {
-        console.log("Can't Find the recipes right now!!, try checking recipi Id or the auth_ids and Keys!!!")
+      }catch (error) {
+        console.log("Can't Find the recipes right now!!, try checking recipi Id or the auth_ids and Keys!!!");
       }
-      
     };
     getData();
   }, []);
