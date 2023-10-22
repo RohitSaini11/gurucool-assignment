9e26142dc35e6b7585fc98cdb753a323b52a7851
Rohit Saini <rs0118032@gmail.com>
2023-10-23 01:51:01 +0530

    Removed API_AUTH_KEYS.js from tracking

--- src/components/API_AUTH_KEYS.js
+++
@@ -1,3 +0,0 @@
-export const api_id="80b7e814";
-export const api_key="417dc50283fb40febbd35850932f36fd";
-export const type="public";
--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,16 +1,28 @@
 import { useEffect, useState } from "react";
+import { api_id,api_key,type } from "./API_AUTH_KEYS";
 
-const Fetchdata = () => {
+const Fetchdata = (props) => {
   const [recipies, setRecipies] = useState([]);
-  const base_url="https://api.edamam.com/api/recipes/v2";
+  const base_url= "https://api.edamam.com/api/recipes/v2";
+  const auth= `app_id=${api_id}&app_key=${api_key}&type=${type}`;
 
   useEffect(() => {
     const getData = async () => {
-      const res = await fetch();
-      if (!res.ok) throw new Error("Oops! An error has occured");
-      const json = await res.json();
-
-      setRecipies(json);
+      try{
+        if(props.id !== ""){ 
+            const res = await fetch(url+'/'+props.id+'?'+auth);
+        }
+        if(props.q !==""){
+            const res = await fetch(`${url}?${type}&q=${props.q}&${auth}`);
+        }
+        if (!res.ok) throw new Error("Oops! An error has occured");
+        const json = await res.json();
+        const data= json.hits;
+        setRecipies(data);
+      } catch (error) {
+        console.log("Can't Find the recipes right now!!, try checking recipi Id or the auth_ids and Keys!!!")
+      }
+      
     };
     getData();
   }, []);
